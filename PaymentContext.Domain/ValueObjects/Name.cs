f810f8a540e0640ec8924fa8f4a03bdd667848c7
Gilmar Oliveira <giilmaroliveira@outlook.com>
2019-06-16 01:12:42 -0300

    Adding Validator by Contract

--- PaymentContext.Domain/ValueObjects/Name.cs
+++ PaymentContext.Domain/ValueObjects/Name.cs
@@ -1,3 +1,4 @@
+using Flunt.Validations;
 using PaymentContext.Shared.ValueObjects;
 
 namespace PaymentContext.Domain.ValueObjects
@@ -9,8 +10,12 @@ namespace PaymentContext.Domain.ValueObjects
             FirstName = firstName;
             LastName = lastName;
 
-            if (string.IsNullOrEmpty(FirstName))
-                AddNotification("Name.FirstName", "Nome Inválido");
+            AddNotifications(new Contract()
+                .Requires()
+                .HasMinLen(FirstName, 3, "Name.FirstName", "Nome deve conter pelo menos 3 caracteres")
+                .HasMinLen(LastName, 3, "LastName.FirstName", "Sobrenome deve conter pelo menos 3 caracteres")
+                .HasMinLen(FirstName, 40, "Name.FirstName", "Nome deve conter até 40 caracteres")
+                );
         }
 
         public string FirstName { get; private set; }
