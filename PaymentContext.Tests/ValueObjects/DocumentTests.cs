5df8a644d6f8bfc596047ae9b2104b6725c58ec5
Gilmar Oliveira <giilmaroliveira@outlook.com>
2019-06-16 09:14:06 -0300

    Adding Student and Document Tests

---
+++ PaymentContext.Tests/ValueObjects/DocumentTests.cs
@@ -0,0 +1,45 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class DocumentTests
+    {
+        // Red, Green, Refactor
+
+        [TestMethod]
+        public void ShouldReturnErrorWhenCNPJIsInvalid()
+        {
+            var doc = new Document("123", EDocumentType.CNPJ);
+            Assert.IsTrue(doc.Invalid);
+        }
+
+        [TestMethod]
+        public void ShouldReturnSuccessWhenCNPJIsValid()
+        {
+            var doc = new Document("12345678912345", EDocumentType.CNPJ);
+            Assert.IsTrue(doc.Valid);
+        }
+
+         [TestMethod]
+        public void ShouldReturnErrorWhenCPFIsInvalid()
+        {
+            var doc = new Document("123", EDocumentType.CPF);
+            Assert.IsTrue(doc.Invalid);
+        }
+
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow("12345678910")]
+        [DataRow("22345678910")]
+        [DataRow("22345678919")]
+        public void ShouldReturnSuccessWhenCPFIsValid(string cpf)
+        {
+            var doc = new Document(cpf, EDocumentType.CPF);
+            Assert.IsTrue(doc.Valid);
+        }
+    }
+}
